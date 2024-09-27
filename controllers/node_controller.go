961f32e112d3b119bb5490f500e0df3189b139ef
shenkonghui <shenkonghui@harmonycloud.cn>
2024-09-27 10:40:26 +0800

    add master multiple

--- controllers/node_controller.go
+++ controllers/node_controller.go
@@ -18,13 +18,14 @@ package controllers
 
 import (
 	"context"
+	"os"
+	"strconv"
+
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog/v2"
-	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"strconv"
 )
 
 // NodeReconciler reconciles a Node object
@@ -75,31 +76,40 @@ func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 		}
 	}
 
+	// 节点扩容倍数
 	multipleStr := os.Getenv("multiple")
 
-	multiple,_ := strconv.Atoi(multipleStr)
-	if multiple == 0{
+	multiple, _ := strconv.Atoi(multipleStr)
+	if multiple == 0 {
 		multiple = 4
 	}
 
+	// master 扩容倍数
+	masterMultipleStr := os.Getenv("master_multiple")
+	masterMultiple, _ := strconv.Atoi(masterMultipleStr)
+	if masterMultiple == 0 {
+		masterMultiple = 1
+	}
+
+	if node.Labels["node-role.kubernetes.io/master"] == "true" {
+		multiple = masterMultiple
+	}
+
 	if needUpdtae {
-		klog.V(5).Infof("Need update: %s",node.Name)
+		klog.V(5).Infof("Need update: %s", node.Name)
 		cpuAllocatable.Set(cpuAllocatable.Value() * int64(multiple))
 		node.Status.Allocatable["cpu"] = *cpuAllocatable
 		node.Status.Capacity["cpu"] = *cpuAllocatable
-		klog.V(5).Infof("scale: cpu %s -> %s",node.Annotations["cpuAllocatable"], cpuAllocatable.String())
+		klog.V(5).Infof("scale: cpu %s -> %s", node.Annotations["cpuAllocatable"], cpuAllocatable.String())
 
 		memAllocatable.Set(memAllocatable.Value() * int64(multiple))
 		node.Status.Allocatable["memory"] = *memAllocatable
 		node.Status.Capacity["memory"] = *memAllocatable
-		klog.V(5).Infof("scale: memory %s -> %s",node.Annotations["memAllocatable"], memAllocatable.String())
+		klog.V(5).Infof("scale: memory %s -> %s", node.Annotations["memAllocatable"], memAllocatable.String())
 
 		r.Client.Status().Update(context.TODO(), &node)
-		//if err != nil {
-		//	return ctrl.Result{}, err
-		//}
 	} else {
-		klog.V(5).Infof("No need update: %s",node.Name)
+		klog.V(5).Infof("No need update: %s", node.Name)
 	}
 
 	return ctrl.Result{}, nil
